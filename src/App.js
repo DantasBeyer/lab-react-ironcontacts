b5d8989a8e80907ee338866fabf57cb53fe60c71
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-06-24 21:00:58 +0200

    all contacts with map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import './App.css';
 import Contacts from "./components/Contacts";
 import contactsJson from "./contacts.json"
  
@@ -7,15 +7,43 @@ import contactsJson from "./contacts.json"
 function App() {
     const [contacts, setContacts] = useState(contactsJson); 
 
+    
   return <div className="App">
-            <Contacts name= {contacts[2].name}
-                      image= {contacts[2].pictureUrl}
-                      popularity={contacts[2].popularity}
-                      id={contacts[2].id}
-                      wonOscar={contacts[2].wonOscar}
-                      wonEmmy={contacts[2].wonEmmy}
-                      />
-                      
+    <h1>IronContacts</h1>
+
+{  contactsJson.map((contacts) => {
+    return (
+      <div>
+          <div className="bar">    
+            <h2>Picture</h2>
+            <h2>Name</h2>
+            <h2>Popularity</h2>
+          </div>
+
+          <div> 
+            <table className="Contacts">
+              <img  className="Picture" src={contacts.pictureUrl} /> 
+              <h4> {contacts.name}</h4>
+              <h4> {contacts.popularity}</h4>
+            </table>
+          </div>
+
+      </div>
+    )
+  })
+}
+
+    {/* <div className="bar">      
+        <h2>Picture</h2>
+        <h2>Name</h2>
+        <h2>Popularity</h2>
+    </div>
+
+      <Contacts name= {contacts[2].name}
+                image= {contacts[2].pictureUrl}
+                popularity={contacts[2].popularity}
+                />
+                     */}  
          </div>;
   
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,21 +5,20 @@ import React from "react";
 
 function Contacts (props){
 
-    <h2>Contacts</h2>
-     
+    
+/*      
     return (
-        <div  className="contacts">
-            <p>Name: {props.name}</p>
-            <img className="Picture" src={props.image} alt="" />
-            <p>Popularity: {props.popularity}</p>
-            <p>Id: {props.id}</p>
+        <div  className="Contacts">
             
-          {/*   must to be fixed */}
-            <p>Oscars: {props.wonOscars ? 'Oscars awarded 🙌' : 'Not Oscars awarded 💆‍♂️'}</p>
-            <p>WonEmmy: {props.wonEmmy}</p>
+
+            <img className="Picture" src={props.image} alt="" />
+            <p>Name: {props.name}</p>         
+            <p>Popularity: {props.popularity}</p>  
+            
+        
         </div>
 
-    )
-}
+    )*/
+} 
 
 export default Contacts;
